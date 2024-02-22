cc28989ed153e166b18f51eff8b638a27566381b
JOAO TARRAN <joaotarran@gmail.com>
2024-02-22 10:33:50 -0300

    seo update test

--- src/pages/expertise/[expertiseKey]/index.tsx
+++ src/pages/expertise/[expertiseKey]/index.tsx
@@ -6,6 +6,60 @@ import expertiseList from "@/content_lists/expertise_list";
 
 import { motion as m, AnimatePresence } from "framer-motion";
 
+const descriptionMap = {
+    direito_do_trabalho: {
+        title: "Direito do Trabalho",
+        description:
+            "Oferecemos orientação jurídica confiável e ágil para empregadores e empregados, abordando questões como contratos, demissões, rescisões e direitos trabalhistas.",
+    },
+    direito_previdenciario: {
+        title: "Direito Previdenciário",
+        description:
+            "Com expertise em Direito Previdenciário, nosso escritório fornece assistência abrangente em questões relacionadas à Previdência Social, incluindo aposentadorias, benefícios por incapacidade, pensões e revisões de benefícios.",
+    },
+    direito_tributario: {
+        title: "Direito Tributário",
+        description:
+            "Especializados em Direito Tributário, oferecemos suporte legal em questões fiscais, incluindo planejamento tributário, contestação de autuações, recuperação de tributos e defesa em processos administrativos e judiciais.",
+    },
+    direito_civil: {
+        title: "Direito Civil",
+        description:
+            "Com vasta experiência em Direito Civil, nossa equipe oferece assistência em diversas áreas, como contratos, responsabilidade civil, direitos reais, sucessões, obrigações e questões relacionadas à propriedade.",
+    },
+    direito_familia: {
+        title: "Direito de Família",
+        description:
+            "Especializados em Direito de Família, prestamos suporte legal em questões como divórcio, pensão alimentícia, guarda de filhos, inventários, adoções, investigação de paternidade e outras questões familiares delicadas.",
+    },
+    direito_sucessoes: {
+        title: "Direito das Sucessões",
+        description:
+            "Com expertise em Direito das Sucessões, oferecemos assistência em processos de inventário, partilha de bens, testamentos, planejamento sucessório e questões relacionadas à herança e patrimônio.",
+    },
+    direito_imobiliario: {
+        title: "Direito Imobiliário",
+        description:
+            "Fornecemos suporte legal em transações imobiliárias, contratos de locação, regularização de propriedades, litígios envolvendo imóveis, condomínios e questões relacionadas ao direito de propriedade.",
+    },
+    direito_empresarial: {
+        title: "Direito Empresarial",
+        description:
+            "Prestamos suporte jurídico a empresas em questões como constituição e dissolução de sociedades, contratos comerciais, recuperação judicial, falência, propriedade intelectual e questões societárias.",
+    },
+    direito_agronegocio: {
+        title: "Direito do Agronegócio",
+        description:
+            "Oferecemos suporte jurídico para empresas e produtores rurais em questões como contratos agrícolas, regularização fundiária, licenciamento ambiental, financiamento agrícola e litígios relacionados ao setor agropecuário.",
+    },
+    direito_bancario: {
+        title: "Direito Bancário",
+        description:
+            "Oferecemos assistência jurídica em questões como contratos bancários, financiamentos, cobranças, defesa do consumidor bancário, disputas contratuais e litígios com instituições financeiras.",
+    },
+    // Add more queries and descriptions as needed
+};
+
 export default function Expertise() {
     const router = useRouter();
     const { expertiseKey } = router.query;
@@ -14,6 +68,8 @@ export default function Expertise() {
     const expertise = expertiseList.find((expertise) => expertise.key === expertiseKey);
 
     const message = "Olá gostaria de fazer a cotação de serviços relacionados a " + expertise?.title + ".";
+    const expertiseKeyDescription = router.query.expertiseKey as keyof typeof descriptionMap;
+    const customDescription = descriptionMap[expertiseKeyDescription] || "Default meta description";
 
     function toUrlValidString(str: string) {
         return encodeURIComponent(str);
@@ -23,8 +79,8 @@ export default function Expertise() {
     return (
         <>
             <Head>
-                <title>{"Stresser & Machado | " + expertise?.title}</title>
-                <meta name="description" content={expertise?.metadescription} />
+                <title>{"Stresser & Machado | " + customDescription.title}</title>
+                <meta name="description" content={customDescription.description} />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
