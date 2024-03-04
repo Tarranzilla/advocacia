093df73ce5c5940700a138c00e40c12606aefd47
JOAO TARRAN <joaotarran@gmail.com>
2024-03-04 17:39:44 -0300

    build fix

--- src/content_lists/english/banner_list_en.tsx
+++ src/content_lists/english/banner_list_en.tsx
@@ -4,6 +4,7 @@ const bannerList_EN: Banner[] = [
     {
         key: "banner1",
         title: "Maximize Your Retirement",
+        service: "whole life review.",
         subtitle: "Discover if you are eligible for the Whole Life Review and increase your retirement now!",
         imgSrc: "/banner_imgs/banner_aposentadoria_001.png",
         size: {
@@ -13,7 +14,8 @@ const bannerList_EN: Banner[] = [
     },
     {
         key: "banner2",
-        title: "Specialized Assistance in INSS Benefits",
+        title: "specialized assistance in INSS benefits.",
+        service: "Social Security",
         subtitle: "Get professional guidance to ensure your social security rights. Count on our experience!",
         imgSrc: "/banner_imgs/banner_inss_001.png",
         size: {
@@ -24,6 +26,7 @@ const bannerList_EN: Banner[] = [
     {
         key: "banner3",
         title: "Facilitate the Probate Process",
+        service: "probate process.",
         subtitle: "Specialized legal support to streamline and simplify the probate process.",
         imgSrc: "/banner_imgs/banner_inventario_001.png",
         size: {
@@ -34,6 +37,7 @@ const bannerList_EN: Banner[] = [
     {
         key: "banner4",
         title: "Protect Your Rights at Work",
+        service: "labor law.",
         subtitle: "Rely on our expertise in Labor Law to defend your interests and ensure your labor rights!",
         imgSrc: "/banner_imgs/banner_trabalhista_001.png",
         size: {
