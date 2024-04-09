5ad3e108bf6916c5b80bbae1109d9ad7ede645a7
JOAO TARRAN <joaotarran@gmail.com>
2024-04-09 11:38:44 -0300

    image supress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,10 @@ export default function Home() {
                                 {t.landingPage.sections.team.members.map((member, index) => (
                                     <div className="Team_Member" key={index}>
                                         <div className="Team_Member_Header">
-                                            <Image src={member.picture} alt={member.name} width={200} height={300} />
+                                            {/*
+                                                <Image src={member.picture} alt={member.name} width={200} height={300} />
+                                            */}
+
                                             <div className="Team_Member_Info">
                                                 <h2 className="Team_Member_Name">{member.name}</h2>
 
